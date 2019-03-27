e38b92c16e19e149b25f27ecc53b2e89e343c422
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-27 14:19:16 -0400

    adding engageSecondaryEngines method to rockets

--- rocket.js
+++ rocket.js
@@ -1,10 +1,14 @@
-var Rocket = function (color, size) {
-  this.color = color;
-  this.size = size;
+var Rocket = function(color, size) {
+  this.color = color
+  this.size = size
 
-  this.launch = function () {
-    console.log('3, 2, 1, blastoff!');
+  this.launch = function() {
+    console.log('3, 2, 1, blastoff!')
+  }
+
+  this.engageSecondaryEngines = function() {
+    console.log('Secondary Engines Engaged!')
   }
 }
 
-module.exports = Rocket;
+module.exports = Rocket
